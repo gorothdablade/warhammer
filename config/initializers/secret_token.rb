d6c172a4636245cbf9eb4a7a3e5e115401a10715
Michael Gorman <michael@noesissoftware.com>
2013-11-25 20:04:51 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WarhammerOnline::Application.config.secret_key_base = '32d03b619e04ff9c454a09eceea97a8961fd040675b51493f9a46b1d488a9dd9361930f020ecf2fe94eb4f90d91aefc2b248c1cee377d65a697efb5ef46910bf'
